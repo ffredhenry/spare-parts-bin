2d644eaa750ddb9138d9ee1991db450ce64ac6d1
fhenry9618 <f.fredhenry@gmail.com>
2022-09-08 12:12:08 -0500

    Added volatility2 installation script (useful for Kali or other forensic boxes)

---
+++ get-volatility2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+# install python2.7 and python2-dev
+cd ~/
+echo "Please enter password when prompted."
+sudo apt update
+sudo apt install python2
+sudo apt install python2-dev
+
+# download pip2 and install
+echo "Setting up 'Scripts' environment..."
+mkdir Scripts
+cd Scripts
+
+wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+sleep 2
+echo "Installing pip2 from script..."
+sudo python2 get-pip.py
+
+# pip2 upgrade for setuptools
+clear
+echo "Installing pip2 packages..."
+pip2 install --upgrade setuptools
+pip2 install pycrypto
+pip2 install distorm3
+
+# clone volatility repo and install
+echo "Setting up Volatility 2..."
+cd ~/
+git clone https://github.com/volatilityfoundation/volatility
+cd volatility
+sudo python setup.py install
