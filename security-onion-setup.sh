997db626e76d0cfe75e4708aa032ae359f1a56a7
internetlandlord <f.fredhenry@gmail.com>
2022-11-11 00:44:52 -0600

    quick and dirty security onoin script

---
+++ security-onion-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Security onion for Ubuntu 20.04 installs
+# DONT RUN AS ROOT but enter password when prompted
+
+# prereqs here
+sudo apt install curl
+
+sudo apt install ethtool
+
+# clone the repo
+git clone https://github.com/Security-Onion-Solutions/securityonion
+
+#move into repo 
+cd securityonion
+
+#start security onion install
+sudo bash so-setup-network
