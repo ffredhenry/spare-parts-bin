0ffe34d51756197175e73689ee5ef1f94e0084c5
Fred <f.fredhenry@gmail.com>
2024-10-19 13:14:45 -0500

    modified sshd-cfg to work with Deb 11

--- sshd-cfg.sh
+++ sshd-cfg.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+#FIXME: check for existence of publickeys or authorized keys before running whole script
 # check for sshd config backup and create one if none found
-if [ ! -e /etc/ssh/sshd_config.backup ]
+if [ ! -e /etc/ssh/sshd_config.BAK ]
 then
     echo "Backing up config file..."    
-    cp /etc/ssh/sshd_config.backup{,.backup}
+    cp /etc/ssh/sshd_config{,.BAK}
 fi
 
-# quick assignement
+# quick assignment
 sshd_cfg="/etc/ssh/sshd_config"
 
 # Make alterations to sshd configuration file [FIXME: remove flags once successful]
-sed -ie 's/^#PermitRootLogin prohibit-password/PermitRootLogin no# flag1/' $sshd_cfg    
+#sed -ie 's/^#PermitRootLogin prohibit-password/PermitRootLogin no# flag1/' $sshd_cfg    
 sed -ie 's/^#PasswordAuthentication yes/PasswordAuthentication no# flag2/' $sshd_cfg
+sed -ie 's/^#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no# flag1/' $sshd_cfg
 sed -ie 's/^UsePAM yes/UsePAM no#flag3/' $sshd_cfg
 #sed -ie 's/^ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' $sshd_cfg
 #[FIXME]: find the final line in the sshd_config file and use 'sed' to append the above line *BELOW* it using '\n'
